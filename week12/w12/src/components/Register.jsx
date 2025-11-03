5664da4dc1dc30b279f4ae7461efba0d56efd1a8
sathya-reddy1658 <ktr6304@gmail.com>
2025-11-04 02:29:36 +0530

    helo

--- week12/w12/src/components/Register.jsx
+++ week12/w12/src/components/Register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
                 /> 
                 <input 
                     type="password"
-                      placeholder="Password" 
+                    placeholder="Password" 
                     value={password} 
                     onChange={(e) => setPassword(e.target.value)} 
                 /> 

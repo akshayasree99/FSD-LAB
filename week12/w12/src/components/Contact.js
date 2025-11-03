ade628ab162b1fdd78cade96b95fc9900471ed4e
Akshaya Sree <akshayasree.puligilla999@gmail.com>
2025-11-04 01:50:15 +0530

    w12

---
+++ week12/w12/src/components/Contact.js
@@ -0,0 +1,12 @@
+ 
+const Contact = () => { 
+    return ( 
+        <div> 
+            <h2>Contact Us</h2> 
+            <p>Email: support@studentsystem.com</p> 
+            <p>Phone: +1 (555) 123-4567</p> 
+        </div> 
+    ); 
+}; 
+ 
+export default Contact;
